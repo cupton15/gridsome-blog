add04a5b36345b3784bb110351929aa46c0aaef4
Carl Upton <carlupton15@gmail.com>
2019-03-31 18:42:55 +0100

    adding the sass!

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 // This is the main.js file. Import global CSS and scripts here.
+import '~/assets/style/index.scss'
+
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
