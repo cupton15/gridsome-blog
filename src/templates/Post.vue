0d24166b3b3dd21a7864adf82456e11ac59373d8
Carl Upton <carlupton15@gmail.com>
2019-04-07 20:47:43 +0100

    small bits working on

---
+++ src/templates/Post.vue
@@ -0,0 +1,25 @@
+<template>
+  <Layout>
+    <h1> {{ $page.post.title }} </h1>
+    <div v-html="$page.post.content" />
+  </Layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<page-query>
+    query Post ($path: String!) {
+        post: post (path: $path) {
+            title,
+            content
+        }
+    }
+</page-query>
+
+<style>
+
+</style>
